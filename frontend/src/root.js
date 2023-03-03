cf3c3f52ebf6a52320b5b7feb5be6be0e1f26f1a
Asha Luckins <ashaluckinsanoop@gmail.com>
2023-03-03 14:16:25 -0800

    remove the time delay in backend

--- frontend/src/root.js
+++ frontend/src/root.js
@@ -2,10 +2,12 @@ import { Outlet } from "react-router-dom";
 import MainNavigation from "./pages/MainNavigation";
 import classes from "./root.module.css";
 const RootLayout = () => {
+  // const navigation = useNavigation();
   return (
     <>
       <MainNavigation />
       <main className={classes.content}>
+        {/* {navigation.state === "loading" && <p>Loading...</p>} */}
         <Outlet />
       </main>
     </>
